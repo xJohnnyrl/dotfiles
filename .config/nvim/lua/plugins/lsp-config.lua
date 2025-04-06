b70098c32e06c68c72764956fa991a246321e56b
Johnny <jonathanrl2003@gmail.com>
2025-04-05 23:59:00 -0400

    Added all configs

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,52 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+        config = function ()
+            require("mason-lspconfig").setup({
+            ensure_installed = {"lua_ls", "tsserver", "html", "cssls", "tailwindcss", "basedpyright"}
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.cssls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+                filetypes={"html"}
+            })
+            lspconfig.basedpyright.setup({
+                capabilities = capabilities
+            })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+}
