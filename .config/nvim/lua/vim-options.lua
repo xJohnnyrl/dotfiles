1dd7376d39af065281f52996d5fade144267960b
Johnny <jonathanrl2003@gmail.com>
2025-05-05 02:40:39 -0400

    Small nvim changes

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -18,3 +18,7 @@ vim.opt.termguicolors = true
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
 vim.opt.scrolloff = 10
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+vim.keymap.set("n", "n", "nzzzv", { silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { silent = true })
